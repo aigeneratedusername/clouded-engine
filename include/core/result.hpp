e69dff8470466ae28b8ff2f1c8c59e4653d38ad0
aigeneratedusername <jaestaytuff@proton.me>
2026-07-14 22:58:47 -0400

    Finished result system

--- include/core/result.hpp
+++ include/core/result.hpp
@@ -8,11 +8,7 @@ enum EStatus {
 
 typedef uint16_t EErrorDomain;
 typedef uint16_t EErrorCode;
-
-struct EError {
-	EErrorDomain 	domain;
-	EErrorCode  	code;
-};
+typedef uint32_t EError;
 
 struct EResult {
 	EStatus status;
@@ -21,3 +17,50 @@ struct EResult {
 		EError 		error;
 	};
 };
+
+#define ERESULT_PTR(T, res) ((T*)(res).data)
+#define ERESULT_UINT(res) 	((uintptr_t)(res).data)
+#define ERESULT_INT(res) 	((intptr_t)(res).data)
+
+// NOTE: DO NOT USE THIS WITH NON EResult TYPES!
+// for PResults use PRESULT_TRY, for function that return neither? 
+// idk go fuck yourself?
+#define ERESULT_TRY(expr, res, ...)		\
+	do { 								\
+		(res) = (expr); 				\
+		if(EFailed((res))) {			\
+			__VA_ARGS__;				\
+		} 								\
+	} while(0) 							\
+
+static inline EResult EErr(EErrorDomain domain, EErrorCode code) {
+	return {
+		.status = ESTATUS_ERROR,
+		.error = ((uint32_t) domain << 16) | (uint32_t) code
+	};
+}
+
+static inline EResult EOk_ptr(void *ptr) {
+	return {
+		.status = ESTATUS_OK,
+		.value = (uintptr_t) ptr
+	};
+}
+
+static inline EResult EOk_int(intptr_t val) {
+	return {
+		.status = ESTATUS_OK,
+		.value = (uintptr_t) val
+	};
+}
+
+static inline EResult EOk_uint(uintptr_t val) {
+	return {
+		.status = ESTATUS_OK,
+		.value = val
+	};
+}
+
+static inline bool EFailed(EResult res) {
+	return res.status != ESTATUS_OK;
+}
